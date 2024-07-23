ae8955d017a4383d4abf0b52ae1e8702d54e8bfe
netaaviram <netaaviram@mail.tau.ac.il>
2024-07-23 09:51:42 +0300

    Update COL_B2T.sv
    This block creates the col_on and col_off signals

--- Software/System Verilog/FLB/DECODER/COL_B2T.sv
+++ Software/System Verilog/FLB/DECODER/COL_B2T.sv
@@ -1,1 +1,20 @@
+module COL_B2T (
+	input  [3:0] bin,   // 4-bit binary input
+	output [15:0] rise_o, // 16-bit thermometer code output
+	output [15:0] fall_o // 16-bit thermometer code output
+);
+
+wire [15:0] th;
+// Instantiate bin_to_thermo to generate rise_o
+B2T B2T_inst (
+	.binary(bin),
+	.thermo(th)
+);
+
+// Generate fall_o as the complement of rise_o
+
+assign rise_o =   {th[14:0],1'b1}; 
+assign fall_o =   {1'b1 ,~th[14:0]};
+
+endmodule
 
