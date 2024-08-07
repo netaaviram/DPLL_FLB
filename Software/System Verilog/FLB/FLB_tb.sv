03627c02a756236513332a28d40a7654eb80d173
netaaviram <netaaviram@mail.tau.ac.il>
2024-08-07 16:23:18 +0300

    Update FLB_tb.sv

--- Software/System Verilog/FLB/FLB_tb.sv
+++ Software/System Verilog/FLB/FLB_tb.sv
@@ -5,25 +5,22 @@ logic nsh_clk;
 logic ref_clk;
 logic [15:0] dlf_out; 
 logic [7:0] band;
-wire [15:0] row_p;
-wire [15:0] row_n;
-wire [15:0] col_on;
-wire [15:0] col_off;
+logic [15:0] band_thrm_hi;
+logic [15:0] band_thrm_lo;
 
 // Registers Inputs (EMAS) 
-logic sdm_on; //Enables noise-shaping  
-logic sdm_order; // 0 for first order, 1 for second order
-logic sdm_thrm_en; // 1 enables thermometric output
-logic sdm_man_on; // enables manually output definition 
-logic [2:0] sdm_man_val; // manually defined value 
-logic [1:0] mtrx_clk_lag; // Picks the number of lag clock cycles for the decoder
-logic [1:0] smpl_clk_lag; // Picks the number of lag clock cycles for input sampling
-logic rst_n;
+logic csr_flb_sdm_on;
+logic csr_flb_sdm_order;
+logic csr_flb_sdm_thrm_en;
+logic csr_flb_sdm_man_on;
+logic csr_flb_rst_n;
+logic [2:0] csr_flb_sdm_man_val;
+logic [1:0] csr_flb_mtrx_clk_lag;
+logic [1:0] csr_flb_smpl_clk_lag;
+
 
 // Outputs 
 logic [2:0] os_thrm;
-logic [63:0] mtrx_thrm;
-logic [255:0] band_thrm;
 logic [7:0] band_bin;
 
 
@@ -33,81 +30,76 @@ FLB uut (
   .ref_clk(ref_clk),
   .dlf_out(dlf_out),
   .band(band),
-  .sdm_on(sdm_on),
-  .sdm_order(sdm_order),
-  .sdm_thrm_en(sdm_thrm_en),
-  .sdm_man_on(sdm_man_on),
-  .sdm_man_val(sdm_man_val),
-  .mtrx_clk_lag(mtrx_clk_lag),
-  .smpl_clk_lag(smpl_clk_lag),
-  .rst_n(rst_n),
+  .csr_flb_sdm_on(csr_flb_sdm_on),
+  .csr_flb_sdm_order(csr_flb_sdm_order),
+  .csr_flb_sdm_thrm_en(csr_flb_sdm_thrm_en),
+  .csr_flb_sdm_man_on(csr_flb_sdm_man_on),
+  .csr_flb_sdm_man_val(csr_flb_sdm_man_val),
+  .csr_flb_mtrx_clk_lag(csr_flb_mtrx_clk_lag),
+  .csr_flb_smpl_clk_lag(csr_flb_smpl_clk_lag),
+  .csr_flb_rst_n(csr_flb_rst_n),
+  //output
   .os_thrm(os_thrm),
-  .mtrx_thrm(mtrx_thrm),
-  .band_thrm(band_thrm),
+  .row_p(row_p),
+  .row_n(row_n),
+  .col_on(col_on),
+  .col_off(col_off),
+  .band_thrm_hi(band_thrm_hi),
+  .band_thrm_lo(band_thrm_lo),
   .band_bin(band_bin)
 );
 
 // Clock generation
 always #5 nsh_clk = ~nsh_clk;
 always #50 ref_clk = ~ref_clk;
-assign col_off = mtrx_thrm[15:0];
-assign col_on = mtrx_thrm[31:16];
-assign row_n = mtrx_thrm[47:32];
-assign row_p = mtrx_thrm[63:48];
 
 initial begin
   // Initialize inputs
-  nsh_clk = 0;
-  ref_clk = 0;
+  nsh_clk = 1'b0;
+  ref_clk = 1'b0;
   dlf_out = 16'h0000;
   band = 8'h00;
-  sdm_on = 0;
-  sdm_order = 0;
-  sdm_thrm_en = 0;
-  sdm_man_on = 0;
-  sdm_man_val = 3'b000;
-  mtrx_clk_lag = 2'b00;
-  smpl_clk_lag = 2'b00;
-  rst_n = 0;
+  csr_flb_sdm_on = 1'b0;
+  csr_flb_sdm_order = 1'b0;
+  csr_flb_sdm_thrm_en = 1'b0;
+  csr_flb_sdm_man_on = 1'b0;
+  csr_flb_sdm_man_val = 3'b000;
+  csr_flb_mtrx_clk_lag = 2'b00;
+  csr_flb_smpl_clk_lag = 2'b00;
+  csr_flb_rst_n = 1'b0;
 
   // Apply reset
   #50;
-  rst_n = 1;
+  csr_flb_rst_n = 1'b1;
 
   // Apply test vectors
   #80;
-  dlf_out = 16'h1234;
+  dlf_out = 16'h1234; 
   band = 8'hAA;
-  sdm_on = 1;
-  sdm_order = 1;
-  sdm_thrm_en = 1;
-  sdm_man_on = 0;
-  sdm_man_val = 3'b000;
-  mtrx_clk_lag = 2'b11;
-  smpl_clk_lag = 2'b00;
+  csr_flb_sdm_on = 1'b1; //noise shaping enabled
+  csr_flb_sdm_order = 1'b1; //second order SDM
+  csr_flb_sdm_thrm_en = 1'b1; //thermometric output enabled
+  csr_flb_sdm_man_on = 1'b0; // manually output disabled
+  csr_flb_sdm_man_val = 3'b000;
+  csr_flb_mtrx_clk_lag = 2'b11;
+  csr_flb_smpl_clk_lag = 2'b00;
 
   #200;
   // Change test vectors
   dlf_out = 16'h5678;
   band = 8'hBB;
-//  sdm_on = 0;
-//  sdm_order = 0;
-//  sdm_thrm_en = 0;
-//  sdm_man_on = 0;
-//  sdm_man_val = 3'b010;
-  mtrx_clk_lag = 2'b10;
-  smpl_clk_lag = 2'b01;
+  csr_flb_mtrx_clk_lag = 2'b10;
+  csr_flb_smpl_clk_lag = 2'b01;
 
   #500;
   // End of simulation
-  $stop;
+ $finish;
 end
 
 // Monitor output signals
-//initial begin
-//  $monitor("Time: %t, os_thrm = %b, row_p = %b, row_n = %b, col_on = %b, col_off = %b, band_thrm = %b", 
-//			$time, os_thrm, row_p, row_n, col_on, col_off, band_thrm);
-//end
+initial begin
+  $monitor("Time: %t, os_thrm = %b, row_p = %b, row_n = %b, col_on = %b, col_off = %b, band_thrm_hi = %b, band_thrm_lo = %b", 
+			$time, os_thrm, row_p, row_n, col_on, col_off, band_thrm_hi, band_thrm_lo);
+end
 
 endmodule
-
